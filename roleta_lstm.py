de0834cf86b60914ba0c6ec49c3a32c00afda7ff
Rovicentini <rodrigo.vicentini457@gmail.com>
2025-08-09 09:36:43 -0300

    Update roleta_lstm.py

--- roleta_lstm.py
+++ roleta_lstm.py
@@ -262,7 +262,7 @@ if len(st.session_state.historico) >= SEQUENCIA_ENTRADA + 1:
     if model_classificacao:
         entrada = np.array(st.session_state.historico[-SEQUENCIA_ENTRADA:]).reshape(1, SEQUENCIA_ENTRADA, 1)
        predicao_softmax = model_classificacao.predict(entrada, verbose=0)
-probs = predicao_softmax[0]
+       probs = predicao_softmax[0]
 
 limite = np.mean(probs) + np.std(probs)
 numeros_selecionados = [i for i, p in enumerate(probs) if p >= limite]
@@ -328,6 +328,7 @@ if len(st.session_state.historico) >= SEQUENCIA_ENTRADA + 1:
     st.sidebar.markdown(f"📊 **Total** | ✅ Acertos: {acertos} | ❌ Erros: {erros} | 🔁 Total: {acertos + erros}")
 else:
     st.info("ℹ️ Insira ao menos 11 números para iniciar a previsão com IA.")
+
 
 
 
