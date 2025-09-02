51cdfa1cee9e447aa1c75a6a43e4c68c40cff840
Rovicentini <rodrigo.vicentini457@gmail.com>
2025-09-02 20:54:42 -0300

    Update roleta_lstm.py

--- roleta_lstm.py
+++ roleta_lstm.py
@@ -1108,7 +1108,7 @@ if st.session_state.last_input is not None:
                 st.session_state.dqn_agent = DQNAgent(
                     state_size=exemplo_estado.shape[0],
                     action_size=NUM_TOTAL
-            )
+                )
 
 
         # Reforço com resultado anterior
@@ -1248,6 +1248,7 @@ for metrica, dados in st.session_state.top_n_metrics.items():
         st.metric(label=metrica, value=f"{acuracia:.2f}%", help=f"Baseado em {dados['total']} previsões.")
     else:
         st.metric(label=metrica, value="N/A")
+
 
 
 
